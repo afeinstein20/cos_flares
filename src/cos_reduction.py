4cd5338b34adfa15eee8c9d3b4eacf5aa8fb3deb
Adina Feinstein <adina.d.feinstein@gmail.com>
2021-09-23 09:50:49 -0400

    updated reduction class

--- src/cos_reduction.py
+++ src/cos_reduction.py
@@ -1,6 +1,7 @@
 import calcos
 import os, sys
 import numpy as np
+from tqdm import tqdm
 from astropy import units
 from astropy.io import fits
 from costools import splittag
@@ -28,16 +29,11 @@ class cosReduce(object):
            of observations.
         input_path : str
            The path to where the raw data is stored.
-        output_path : str
-           The path to where the output data will be stored.
-           This will create a subdirectory `a` and `b` when
-           necessary.
 
         Attributes
         ----------
         rootname : np.array
         input_path : str
-        output_path : str
         """
         self.rootname = rootname
         self.input_path = input_path
@@ -66,6 +62,11 @@ class cosReduce(object):
         time_list : str, optional
            Comma- or blank-separated string of times for intervals
            if you wish the intervals to be uneven. Default is None.
+
+        Attributes
+        ----------
+        splittag_path : str
+           The path to where the splittag data is saved.
         """
         
         for i in range(len(self.rootname)):
@@ -84,6 +85,7 @@ class cosReduce(object):
                               increment=increment, starttime=starttime,
                               endtime=endtime, time_list=time_list)
                               
+        self.splittag_path = output_path
         return
 
     def check_ref_files(self, path=None):
@@ -141,3 +143,49 @@ class cosReduce(object):
         else:
             return("You have all reference files needed!")
             
+
+    def reduce_data(self, output_path):
+        """
+        Uses the calcos package to reduce new data.
+
+        Parameters
+        ----------
+        output_path : str
+           Where the output reduced data should be stored.
+           This will create a subdirectory `a` and `b` when
+           necessary.
+
+        Attributes
+        ----------
+        reduced_path : str
+           Where the reduced data is stored.
+        """
+        path_a = os.mkdir(os.path.join(output_path, 'a'))
+        path_b = os.mkdir(os.path.join(output_path, 'b'))
+
+        if os.path.exists(output_path):
+            if not os.path.exists(path_a):
+                os.mkdir(path_a)
+            if not os.path.exists(path_b):
+                os.mkdir(path_b)
+
+        else:
+            try:
+                os.mkdir(output_path)
+                os.mkdir(path_a)
+                os.mkdir(path_b)
+            except OSError:
+                return("Couldn't find or create the output_path.")
+                         
+        for letter in ['a', 'b']:
+            if letter == 'a':
+                outdir = path_a
+            else:
+                outdir = path_b
+            split_files = np.unique(np.sort([os.path.join(self.splittag_path, i)
+                                             for i in os.listdir(self.splittag_path)
+                                             if i.endswith('{}.fits'.format(letter))]))
+            for f in tqdm(range(len(split_files))):
+                calcos.calcos(split_files[f], verbosity=0,
+                              outdir=outdir)
+
