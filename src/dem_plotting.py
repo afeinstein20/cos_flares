bb40407f20eb36ffcc3b08a96ccbb36c515e40bc
Adina Feinstein <adina.d.feinstein@gmail.com>
2022-05-23 09:55:02 -0500

    updated everything (completed before paper submission)

--- src/dem_plotting.py
+++ src/dem_plotting.py
@@ -35,14 +35,14 @@ def plot_hdl_dem(path, ax1, c='k', label='', ax2=None, alpha=0.5):
         for i, w in enumerate(flare[line]['centers']):
             try:
                 if ax2 is not None:
-                    ax2.errorbar(np.log10(flare[line]['peakFormTemp'][i]), 
-                                 np.log10(flare[line]['avgDEM'][i]), 
+                    ax2.errorbar(np.log10(flare[line]['peakFormTemp'][i]),
+                                 np.log10(flare[line]['avgDEM'][i]),
                                  fmt='o', color=c)
-                ax1.errorbar(np.log10(quies[line]['peakFormTemp'][i]), 
-                             np.log10(quies[line]['avgDEM'][i]), 
+                ax1.errorbar(np.log10(quies[line]['peakFormTemp'][i]),
+                             np.log10(quies[line]['avgDEM'][i]),
                              fmt='o', color=c, markeredgecolor='k', ms=7)
-                
-                if ((np.log10(flare[line]['peakFormTemp'][i])>6) and 
+
+                if ((np.log10(flare[line]['peakFormTemp'][i])>6) and
                     (np.log10(flare[line]['avgDEM'][i])>28)):
                     print(line, quies[line]['centers'][i])
                 if np.log10(flare[line]['avgDEM'][i])>35:
@@ -51,18 +51,18 @@ def plot_hdl_dem(path, ax1, c='k', label='', ax2=None, alpha=0.5):
             except:
                 print(line)
                 pass
-    
+
     if ax2 is not None:
-        ax2.plot(flare['EUV']['DEMUV']['Chebyshev']['Trange'], 
-                 flare['EUV']['DEMUV']['Chebyshev']['Fit'][1], 
+        ax2.plot(flare['EUV']['DEMUV']['Chebyshev']['Trange'],
+                 flare['EUV']['DEMUV']['Chebyshev']['Fit'][1],
                  lw=3, color=c, zorder=3,
                  alpha=alpha)
-    
-    ax1.plot(quies['EUV']['DEMUV']['Chebyshev']['Trange'], 
-             quies['EUV']['DEMUV']['Chebyshev']['Fit'][1], 
+
+    ax1.plot(quies['EUV']['DEMUV']['Chebyshev']['Trange'],
+             quies['EUV']['DEMUV']['Chebyshev']['Fit'][1],
              lw=3, color=c,  zorder=3, label=label,
              alpha=alpha)
-    
+
     return
 
 
@@ -89,7 +89,7 @@ def plot_binned_spectrum(w, f, e, distance, ax, label='',
        Default is [0,4000] in 1 AA binsizes.
     c : str, optional
        Color to plot in. Default is black.
-    
+
     Returns
     -------
     None
@@ -109,7 +109,7 @@ def plot_binned_spectrum(w, f, e, distance, ax, label='',
     flux = flux*4*np.pi*distance**2
     q = flux > 0
     # plot errorbar function
-    ax.errorbar(centers[q], 
+    ax.errorbar(centers[q],
                 flux[q], yerr=err[q]*4*np.pi*distance**2,
                 marker='o', c=c, ms=10, label=label, linestyle='')
     return
@@ -126,17 +126,17 @@ def compare_with_data(dem_wave, dem_flux, dem_err,
     ----------
     dem_wave : np.array
        Array of DEM wavelengths.
-    dem_flux : np.array 
+    dem_flux : np.array
        Array of DEM fluxes.
-    data_wave : np.array 
+    data_wave : np.array
        Array of observed wavelengths.
-    data_flux : np.array 
+    data_flux : np.array
        Array of observed fluxes.
     lines : np.array
        Array of centers of lines to evaluate over.
     distance : float
        Distance to star. Used to convert flux density to flux.
-    ax : matplotlib.axes._subplots.AxesSubplot 
+    ax : matplotlib.axes._subplots.AxesSubplot
        Axis to plot the data on.
     c : str, optional
        Color to plot the data with. Default is black.
@@ -153,6 +153,9 @@ def compare_with_data(dem_wave, dem_flux, dem_err,
     if binsize is None:
         binsize = np.full(len(lines), 1)
 
+    fdata = np.zeros(len(lines))
+    fdem = np.zeros(len(lines))
+
     for i in range(len(lines)):
         dem_q = ( (dem_wave > lines[i]-binsize[i]) &
                   (dem_wave < lines[i]+binsize[i]) )
@@ -166,10 +169,40 @@ def compare_with_data(dem_wave, dem_flux, dem_err,
         flux_data = np.trapz(data_flux[data_q], data_wave[data_q]) * 4*np.pi*distance**2
         err_data  = np.trapz(data_err[data_q], data_wave[data_q]) * 4*np.pi*distance**2
 
-        ax.errorbar(np.log10(flux_data), 
-                    np.log10(flux_dem), 
+        ax.errorbar(np.log10(flux_data),
+                    np.log10(flux_dem),
                     #xerr=np.abs(np.log10(flux_data) - np.log10(err_data)),
                     #yerr=np.abs(np.log10(flux_dem) - np.log10(err_dem)),
                     marker='o', c=c, ms=8, linestyle='')
+        fdata[i] = np.log10(flux_data)
+        fdem[i] = np.log10(flux_dem)
 
-    return
+    return fdata, fdem
+
+
+def compare_with_table(dem_wave, dem_flux, dem_err,
+                       table, wavekey, fluxkey,
+                       distance, ax, c='k', label='',
+                       binsize=None):
+    if binsize is None:
+        binsize = np.full(len(table), 1)
+
+    fdata = np.zeros(len(table))
+    fdem = np.zeros(len(table))
+
+    for i in range(len(table)):
+        dem_q = ( (dem_wave > table[wavekey][i]-binsize[i]) &
+                  (dem_wave < table[wavekey][i]+binsize[i]) )
+
+        flux_dem  = np.trapz(dem_flux[dem_q], dem_wave[dem_q]) * 4*np.pi*distance**2
+        err_dem   = np.trapz(dem_err[dem_q], dem_wave[dem_q])  * 4*np.pi*distance**2
+
+        ax.errorbar(np.log10(table[fluxkey][i]*4*np.pi*distance**2),
+                    np.log10(flux_dem),
+                    #xerr=np.abs(np.log10(flux_data) - np.log10(err_data)),
+                    #yerr=np.abs(np.log10(flux_dem) - np.log10(err_dem)),
+                    marker='o', c=c, ms=8, linestyle='')
+        fdem[i] = np.log10(flux_dem)
+        fdata[i] = np.log10(table[fluxkey][i]*4*np.pi*distance**2)
+
+    return fdata, fdem
